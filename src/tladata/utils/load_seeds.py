12af8759bea41e725dbeb9eae95ff821f91d29ca
bortiz-101 <bortiz101@hotmail.com>
2026-03-07 19:39:17 -0600

    fixes linter errors

--- src/tladata/utils/load_seeds.py
+++ src/tladata/utils/load_seeds.py
@@ -1,14 +1,13 @@
-from typing import List
 import yaml
 
 
-def load_seed_repos() -> List[str]:
+def load_seed_repos() -> list[str]:
     with open("config/seeds/repos.yaml") as f:
         data = yaml.safe_load(f)
         return data if isinstance(data, list) else []
 
 
-def load_queries() -> List[str]:
+def load_queries() -> list[str]:
     with open("config/seeds/queries.yaml") as f:
         data = yaml.safe_load(f)
         return data if isinstance(data, list) else []
